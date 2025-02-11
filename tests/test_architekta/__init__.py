13fce6ea2120d300709641fe82770bb781752c74
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-02-11 11:01:37 +0100

    refactor(*): Initialize `src` and `tests` directories

---
+++ tests/test_architekta/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+test_architekta
+===============
+
+Tests for the architekta package.
+
+Modules
+-------
+
+
+See Also
+--------
+architekta
+
+Notes
+-----
+This test package is imported by Sphinx to extract docstrings for documenting tests. This
+documentation provides additional examples and explanations for the tested modules and functions.
+"""
