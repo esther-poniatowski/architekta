366a25765604501c2dec74f72423fcec6d023718
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-02-14 11:54:13 +0100

    feat(core): Initialize core module

---
+++ src/architekta/core/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+architekta.core
+===============
+
+Core functionality for the architekta package.
+
+Modules
+-------
+
+See Also
+--------
+
+"""
---
+++ tests/test_architekta/test_core/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+test_architekta.test_core
+=========================
+
+Tests for the core functionality of the architekta package.
+
+Modules
+-------
+
+
+See Also
+--------
+architekta.core
+"""
