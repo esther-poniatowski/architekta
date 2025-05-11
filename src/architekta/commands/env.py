5ee5e9da7443c73fbadf5a28ccc9b592826c9d02
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-05-11 14:21:08 +0200

    feat(src): Initialize future commands for environment management

---
+++ src/architekta/commands/env.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+architekta.commands.env
+=======================
+
+Commands for managing the Conda environment.
+
+Commands
+--------
+
+
+See Also
+--------
+typer : https://typer.tiangolo.com/
+    Library for building CLI applications.
+"""
+
+import typer
+
+app = typer.Typer()
+
+@app.command("setup-deps")
+def setup_deps():
+    """Initialize a Conda environment based on the configuration."""
+    pass
+
+@app.command("update-deps")
+def update_deps():
+    """Update the dependencies of the Conda environment."""
+    pass
+
+@app.command("setup-paths")
+def setup_paths():
+    """Initialize the paths in the Conda environment."""
+    pass
+
+@app.command("update-paths")
+def update_paths():
+    """Update the paths in the Conda environment."""
+    pass
+
+@app.command("inspect-paths")
+def inspect_paths():
+    """Inspect the current Conda environment."""
+    pass
+
+@app.command("inspect-deps")
+def inspect_deps():
+    """Inspect the current Conda environment."""
+    pass
