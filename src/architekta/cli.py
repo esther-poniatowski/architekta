e1d5ec52cd7a9bbd3487dcd75f2d68b2a6907dcd
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-06-08 16:05:07 +0200

    feat(cli): Add root cli module

--- src/architekta/cli.py
+++ src/architekta/cli.py
@@ -1,55 +1,37 @@
 """
-architekta.cli
-==============
+Command-line interface for the `architekta` package.
 
-Command-line interface for the architekta package.
+Defines commands available via `python -m architekta` or `architekta` if installed as a script.
 
 Commands
 --------
-Commands are organized hierarchically into groups (sub-commands) corresponding to specific project
-tasks:
-
-Warning
--------
-The logical groups do not directly correspond to the modules in which the commands are defined.
-Rather, groups are composed by combining commands from different modules. Modules are organized by
-tool (e.g. `env` for conda, `repo` for git and GitHub, etc.) rather than by high-level task (e.g.
-setup, inspect, etc.). Each high-level task may involve multiple tools (e.g. setup env, setup repo,
-etc.).
-
+info : Display diagnostic information.
 
 See Also
 --------
-typer : https://typer.tiangolo.com/
-    Library for building CLI applications.
+typer.Typer
+    Library for building CLI applications: https://typer.tiangolo.com/
 """
 
 import typer
+from . import info, __version__
 
-# from architekta.commands.env import app as env_app
-
-# Main Typer app
-app = typer.Typer(no_args_is_help=True)  # no arg -> show help
-
-# Command groups
-inspect_app = typer.Typer()
-app.add_typer(inspect_app, name="inspect", help="Inspect project's state.")
-
-setup_app = typer.Typer()
-app.add_typer(setup_app, name="setup", help="Setup project components, structure and configurations.")
-
-update_app = typer.Typer()
-app.add_typer(update_app, name="update", help="Update project components, structure and configurations.")
-
-run_app = typer.Typer()
-app.add_typer(run_app, name="run", help="Run code analysis for quality, tests, and documentation.")
-
-version_app = typer.Typer()
-app.add_typer(version_app, name="version", help="Manage versioning and releases.")
-
-dist_app = typer.Typer()
-app.add_typer(dist_app, name="dist", help="Manage project distribution.")
+app = typer.Typer(add_completion=False, no_args_is_help=True)
 
 
-if __name__ == "__main__":
-    app()
+@app.command("info")
+def cli_info() -> None:
+    """Display version and platform diagnostics."""
+    typer.echo(info())
+
+
+@app.callback()
+def main_callback(
+    version: bool = typer.Option(
+        False, "--version", "-v", help="Show the package version and exit."
+    )
+) -> None:
+    """Root command for the package command-line interface."""
+    if version:
+        typer.echo(__version__)
+        raise typer.Exit()
